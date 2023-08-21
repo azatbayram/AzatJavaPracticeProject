44ab8d90dcdabf2eb7c4311c85196dea982d0ee0
azatbayram <bayramov.azat.tkm@gmail.com>
2023-08-21 18:55:55 +0500

    task 61 calculate age

---
+++ src/Java/day13_MethodsPart1/task61_calculateAge.java
@@ -0,0 +1,14 @@
+package Java.day13_MethodsPart1;
+
+public class task61_calculateAge {
+
+    public static void main(String[] args) {
+
+        calculateAge(1995);
+
+    }
+
+    public static void calculateAge(int birthYear){
+        System.out.println("Your age is " + (2023-birthYear));
+    }
+}
