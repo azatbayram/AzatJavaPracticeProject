3a03566fe514a55c4c8d77002d90b3131808d0ad
azatbayram <bayramov.azat.tkm@gmail.com>
2023-08-22 23:19:35 +0500

    ragged array

---
+++ src/Java/day27_array_lab/RaggedArray.java
@@ -0,0 +1,17 @@
+package Java.day27_array_lab;
+
+public class RaggedArray {
+
+    public static void main(String[] args) {
+
+        int [][] array=new int[4][];
+
+        array[0]=new int[3];
+        array[1]=new int[4];
+        array[2]=new int[5];
+        array[3]=new int[2];
+
+        int[][] array2= { {0,0,0},{0,0,0,0},{0,0,0,0,0},{0,0} };
+
+    }
+}
