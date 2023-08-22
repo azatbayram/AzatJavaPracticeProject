267eec97cd69f6005bcaa56e5879ad6b33eb7ae0
azatbayram <bayramov.azat.tkm@gmail.com>
2023-08-22 23:31:42 +0500

    array team

---
+++ src/Java/day27_array_lab/Teams.java
@@ -0,0 +1,30 @@
+package Java.day27_array_lab;
+
+public class Teams {
+
+	public static void main(String[] args) {
+		/*
+		String[][] teams=new String[2][4];
+		
+		teams[0][0]="Mike";
+		teams[0][1]="Tonny";
+		teams[0][2]="Smith";
+		teams[0][3]="Evan";
+		
+		teams[1][0]="David";
+		teams[1][1]="Emmy";
+		teams[1][2]="John";
+		teams[1][3]="Ryan";
+		*/
+		
+		String [][] teams= { {"Mike","Tonny", "Smith","Evan"} , {"David","Emmy","John","Ryan"} };
+		System.out.println("First player of first team");
+		System.out.println(teams[0][0]);
+		
+		System.out.println("number of rows: " + teams.length);
+		System.out.println("People in first team: " + teams[0].length);
+		System.out.println("People in second team: " + teams[1].length);
+
+	}
+
+}
