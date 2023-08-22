1a17bb1940b3f918859ea087c660caca3e1a4f62
azatbayram <bayramov.azat.tkm@gmail.com>
2023-08-22 23:30:36 +0500

    task 95

---
+++ src/Java/day27_array_lab/task95.java
@@ -0,0 +1,24 @@
+package Java.day27_array_lab;
+
+public class task95 {
+
+    public static void main(String[] args) {
+
+        int[][] scores={{68,75,54,80}, {100, 64, 20,50}, {10, 35, 40, 90}};
+        int sum=0;
+
+        for (int column=0; column<scores[0].length; column++){
+            sum=sum+scores[0][column];
+        }
+
+        System.out.println(sum / scores[0].length);
+
+        int sum2=0;
+
+        for (int row=0; row< scores.length; row++){
+            sum2=sum2+scores[row][0];
+        }
+
+        System.out.println(sum2);
+    }
+}
