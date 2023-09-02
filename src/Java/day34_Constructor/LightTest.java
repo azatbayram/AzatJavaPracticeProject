72333c225bbd33cef1cf16d1e49d4d2713bf32c7
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-03 03:45:24 +0500

    light test example

---
+++ src/Java/day34_Constructor/LightTest.java
@@ -0,0 +1,25 @@
+package Java.day34_Constructor;
+
+public class LightTest {
+
+	public static void main(String[] args) {
+		
+		Light l1=new Light();
+		//JVM sees new keyword - invoke constructor
+		//which constructor?
+		
+		//this(); - need to call another constructor
+		//which constructor?		
+		
+		//this(); - need to call another constructor
+		//which constructor?
+		
+		//print return from 3
+		
+		//JVM is asking - where should I go back from here?
+		
+		System.out.println(l1.noOfWatts);
+
+	}
+
+}
