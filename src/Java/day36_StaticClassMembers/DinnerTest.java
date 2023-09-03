911b525c6698d1e63ab08628e8e914e6877ad18d
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-03 06:12:25 +0500

    Dinner test class

---
+++ src/Java/day36_StaticClassMembers/DinnerTest.java
@@ -0,0 +1,26 @@
+package Java.day36_StaticClassMembers;
+
+public class DinnerTest {
+
+	public static void main(String[] args) {
+		
+		Dinner mom=new Dinner();
+		Dinner kid=new Dinner();
+		Dinner dad=new Dinner();
+		
+		System.out.println("Total slices: " + Dinner.pizzaSlice);
+		
+		dad.takeASlice();
+		kid.takeASlice();
+		mom.takeASlice();
+		
+		System.out.println("Total slices: " + Dinner.pizzaSlice);
+		
+		kid.takeASlice(3);
+		dad.takeASlice(2);
+		
+		System.out.println(Dinner.pizzaSlice);
+
+	}
+
+}
