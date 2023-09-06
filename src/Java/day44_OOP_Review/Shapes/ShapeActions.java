8a1130eb1745fe6090719035dfb5f0138905a326
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-07 01:57:47 +0500

    bad array exception learning

--- src/Java/day44_OOP_Review/Shapes/ShapeActions.java
+++ src/Java/day44_OOP_Review/Shapes/ShapeActions.java
@@ -3,26 +3,26 @@ package Java.day44_OOP_Review.Shapes;
 import java.util.ArrayList;
 import java.util.List;
 
-import static day44_OOP_Review.Shapes.ShapeManager.*;
+import static Java.day44_OOP_Review.Shapes.ShapeManager.*;
 
 public class ShapeActions {
 
 	public static void main(String[] args) {
-		
-		
+
+
 		drawSquare((Square)buildShape("square"));
 		
 		drawShape(buildShape("shape"));
-		
+
 		System.out.println("-------------");
-		
+
 		List<Shape> myList = new ArrayList<>();
-		myList.add(new Shape()); 
+		myList.add(new Shape());
 		myList.add(new Square());
 		myList.add(new Shape());
 		myList.add(new Square());
 		myList.add(new Diamond());
-		
+
 		drawShape(myList);
 
 	}
---
+++ src/Java/day45_Exceptions/BadArray.java
@@ -0,0 +1,13 @@
+package Java.day45_Exceptions;
+
+public class BadArray {
+
+    public static void main(String[] args) {
+
+        int[] nums={1,2,3};
+
+        for(int i=0; i<= nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
